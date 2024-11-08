a5ed53168a442076074730d8d1ea6ccca5673f59
engenovic <engenovic@gmail.com>
2024-11-08 10:36:43 +0300

    Added serializers

--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -1,26 +1,15 @@
 from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.views import APIView
+
+# Create your views here.
+
 from .models import Book
+from .serializers import BookSerializer
+from rest_framework import generics
 
+class BookView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
-@api_view(['GET','POST'])
-def books(request):
-    return Response('books',status=status.HTTP_200_OK)
-class BookList(APIView):
-    def get(self,request):
-        author = request.GET.get('author')
-        if(author):
-            return Response({"message":"List of boooks by:" + author},status.HTTP_200_OK)
-        return Response({"message":"List of boooks"},status.HTTP_200_OK)
-        
-    def post(self,request):
-        return Response({"title":request.data.get('title')},status.HTTP_201_CREATED)
-
-class Book(APIView):
-    def get(self, request,pk):
-         return Response({"message":"single book with id " + str(pk)},status.HTTP_200_OK)
-    def put(self,request,pk):
-        return Response({"title":request.data.get('title')},status.HTTP_200_OK)
+class SingleBookView(generics.RetrieveUpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
